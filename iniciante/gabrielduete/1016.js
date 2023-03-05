4c37087f8d1d7f3f82e5a015aba2e3d2abd23550
Hugo Rafael <hugorafell@gmail.com>
2023-03-05 12:01:24 -0300

    Alterando arquivos para focar na explicação

--- iniciante/gabrielduete/1016.js
+++
@@ -1,7 +0,0 @@
-const input = require('fs').readFileSync('/dev/stdin', 'utf8')
-
-const Y = parseInt(input)
-
-const calculo = Y * 2
-
-console.log(calculo + ' minutos')
--- iniciante/rafaelcarvalho/1002.js
+++
@@ -1,9 +0,0 @@
-var input = require('fs').readFileSync('/dev/stdin', 'utf8')
-
-var r = parseFloat(input)
-
-var n = 3.14159
-
-var calculo = n * Math.pow(r, 2)
-
-console.log('A=' + calculo.toFixed(4))
